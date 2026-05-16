c4c4081130b6fe5e092092e128e258d382410721
Ariel Liu <a445liu@uwaterloo.ca>
2026-05-16 14:42:00 -0400

    ran and passed pylint tests

--- modules/quick_trial.py
+++ modules/quick_trial.py
@@ -11,7 +11,7 @@ from detect_colours import DetectBlue, DetectRed
 # Output results of colour detections
 OUTPUT_PATH = pathlib.Path("Output")
 OUTPUT_PATH.mkdir(parents=True, exist_ok=True)
-IMAGE = "modules\map_test.jpg"
+IMAGE = r"modules\map_test.jpg"
 
 # labels for red and blue colour detections
 BLUE_DETECTION = OUTPUT_PATH / f"blue_colour_detection_{time.time_ns()}.jpg"
